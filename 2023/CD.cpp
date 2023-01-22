70264ce47c74751f8d0570d0b566b9e3766ca783
OscarCampohermoso <oscarcampohermoso8@gmail.com>
2023-01-21 20:26:38 -0400

    Primer ejercicio ICPC UCB

--- 2023/CD.cpp
+++ 2023/CD.cpp
@@ -5,5 +5,22 @@ using namespace std;
 #include "bits/stdc++.h"
 
 int main() {
-
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N, M;
+    while (true){
+        cin >> N >> M;
+        if(N == 0 && M == 0){
+            break;
+        }
+        unordered_set <int> cds;
+        cds.clear();
+        int v, cont = 0;
+        for (int i=0; i<N+M; i++){
+            cin >> v;
+            if(!cds.insert(v).second)
+                cont++;
+        }
+        cout << cont << "\n";
+    }
 }
